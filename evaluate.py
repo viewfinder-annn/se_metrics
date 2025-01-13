a6c9f47c84e717df0ffaf63f04c185a749b5b865
annn <annnmusic37@gmail.com>
2025-01-13 11:48:10 +0800

    upload

--- evaluate.py
+++ evaluate.py
@@ -17,32 +17,40 @@ def evaluate_from_audio(noisy_dir, clean_dir, saved_dir, target_sr=None):
     for audio in tqdm(audio_list):
         saved_path = os.path.join(saved_dir, audio)
         clean_path = os.path.join(clean_dir, audio)
-        est_audio, sr = sf.read(saved_path)
-        clean_audio, sr = sf.read(clean_path)
+        est_audio, est_sr = torchaudio.load(saved_path)
+        clean_audio, clean_sr = torchaudio.load(clean_path)
         # Resample to target_sr
         if target_sr is not None:
-            est_audio = torchaudio.transforms.Resample(sr, target_sr)(torch.tensor(est_audio).unsqueeze(0)).squeeze().numpy()
-            clean_audio = torchaudio.transforms.Resample(sr, target_sr)(torch.tensor(clean_audio).unsqueeze(0)).squeeze().numpy()
+            est_audio = torchaudio.transforms.Resample(est_sr, target_sr)(est_audio)
+            clean_audio = torchaudio.transforms.Resample(clean_sr, target_sr)(clean_audio)
+            est_audio = est_audio.squeeze().numpy()
+            clean_audio = clean_audio.squeeze().numpy()
+            # est_audio = est_audio[:len(clean_audio)]
         else:
             target_sr = sr
-        metrics = compute_metrics(clean_audio, est_audio, target_sr, 0)
-        metrics = np.array(metrics)
-        metrics_total += metrics
+        try:
+            metrics = compute_metrics(clean_audio, est_audio, target_sr, 0)
+            metrics = np.array(metrics)
+            metrics_total += metrics
+        except Exception as e:
+            print(e)
+            print("Error in computing metrics for", audio)
+            continue
 
     metrics_avg = metrics_total / num
     metrics_avg_dict = {
-        "pesq": metrics_avg[0],
-        "csig": metrics_avg[1],
-        "cbak": metrics_avg[2],
-        "covl": metrics_avg[3],
-        "ssnr": metrics_avg[4],
-        "stoi": metrics_avg[5],
+        "pesq": metrics_avg[0].round(3),
+        "csig": metrics_avg[1].round(3),
+        "cbak": metrics_avg[2].round(3),
+        "covl": metrics_avg[3].round(3),
+        "ssnr": metrics_avg[4].round(3),
+        "stoi": metrics_avg[5].round(3),
     }
-    
+    print(noisy_dir, clean_dir, saved_dir)
     print(metrics_avg_dict)
     
-    with open(os.path.join(saved_dir, "_metrics.json"), "w") as f:
-        json.dump(metrics_avg_dict, f, indent=4)
+    # with open(os.path.join(saved_dir, "_metrics.json"), "w") as f:
+    #     json.dump(metrics_avg_dict, f, indent=4)
 
 if __name__ == "__main__":
 
---
+++ run.sh
@@ -0,0 +1,8 @@
+# python ./tools/dnsmos.py -t /mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/urgentchallenge2024-55M-20240712-11:53/infer/20240712-15:23/enhanced_16000 -o /mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/urgentchallenge2024-55M-20240712-11:53/infer/20240712-15:23/result.csv -d /mnt/data2/zhangjunan/urgent2024_challenge/DNSMOS/DNSMOS
+
+# python ./tools/dnsmos.py -t /mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/urgentchallenge2024-55M-20240712-11:53/infer/20240712-15:27/enhanced_16000 -o /mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/urgentchallenge2024-55M-20240712-11:53/infer/20240712-15:27/result.csv -d /mnt/data2/zhangjunan/urgent2024_challenge/DNSMOS/DNSMOS
+
+# python ./tools/dnsmos.py -t /mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/urgentchallenge2024-55M-20240712-11:53/infer/20240712-15:28/enhanced_16000 -o /mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/urgentchallenge2024-55M-20240712-11:53/infer/20240712-15:28/result.csv -d /mnt/data2/zhangjunan/urgent2024_challenge/DNSMOS/DNSMOS
+
+
+python ./tools/dnsmos.py -t /mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/urgentchallenge2024-55M-20240712-11:53/infer/20240712-16:07/enhanced -o /mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/urgentchallenge2024-55M-20240712-11:53/infer/20240712-16:07/dnsmos.csv -d /mnt/data2/zhangjunan/urgent2024_challenge/DNSMOS/DNSMOS
